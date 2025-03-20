09c55089079aa729f692373371847dfe53f62253
Miguel Shema Ngabonziza <shemamiguel2023@gmail.com>
2025-03-19 21:40:31 -0500

    Commit2

---
+++ backend_sanity/schemaTypes/brands.ts
@@ -0,0 +1,23 @@
+import { defineType } from 'sanity'
+
+export const brands = defineType {
+    name:'brands',
+    title:'Brands',
+    type: 'document',
+    fields:[
+        {
+            name:'imgUrl',
+            title:'ImgUrl',
+            type: 'image',
+            options: {
+              hotspot: true,
+            },
+        },
+        {
+            name:'name',
+            title:'Name',
+            type:'string'
+        }
+    ]
+}
+);
---
+++ backend_sanity/schemaTypes/index.ts
@@ -0,0 +1,15 @@
+import { abouts } from './abouts';
+import { contact } from './contact';
+import { experiences } from './experiences';
+import { testimonials } from './testimonials';
+import { workExperience } from './workExperience';
+import { works } from './works';
+
+export const schemaTypes = [
+    abouts,
+    contact,
+    workExperience,
+    experiences,
+    works,
+    testimonials
+]
